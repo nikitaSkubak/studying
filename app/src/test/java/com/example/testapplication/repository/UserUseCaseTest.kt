1ce7c0464055fe5bcf098380871f3053b068b05c
meansWorker <mykyta.skubak@coreteka.com>
2021-01-14 10:05:44 +0200

    feat(project) added project to github.

--- app/src/test/java/com/example/testapplication/repository/UserUseCaseTest.kt
+++ app/src/test/java/com/example/testapplication/repository/UserUseCaseTest.kt
@@ -1,11 +1,10 @@
-package com.example.testapplication.usecase
+package com.example.testapplication.repository
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.testapplication.api.PlaceHolderAddress
 import com.example.testapplication.api.PlaceHolderGeo
 import com.example.testapplication.api.PlaceHolderUser
-import com.example.testapplication.repository.UserRepository
 import com.example.testapplication.usecase.user.UserUseCase
 import com.example.testapplication.usecase.user.impl.UserUseCaseImpl
 import com.example.testapplication.util.toUser
@@ -16,7 +15,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
